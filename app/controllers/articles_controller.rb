0d2da0310a8124daca382373f3a6130e6d402efe
MaksimDudar <maksimdudar1@gmail.com>
2022-09-02 19:51:57 +0300

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @articles = Article.all
+    @article = Article.find(1)
     @article = Article.new(article_params)
     if @article.valid?
       @article.save
