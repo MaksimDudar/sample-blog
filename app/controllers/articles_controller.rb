f7dec401d8ebd356ae50ccf60119dbe8aa8e3ade
MaksimDudar <maksimdudar1@gmail.com>
2022-09-12 00:41:26 +0300

    created_at: :desc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
   def index
       @articles = Article.all
-
+      @articles = Article.order(created_at: :desc)
   end
 
   def show
