9afcf2b47c6cbead6fa80a78787ea12b2a467bd9
MaksimDudar <maksimdudar1@gmail.com>
2022-10-03 23:33:41 +0300

    update

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,10 +1,10 @@
 def sign_up
   visit new_user_registration_path
 
-  fill_in :user_email, :with => 'user@example.ru'
-  fill_in :user_username, :with => 'dimas'
-  fill_in :user_password, :with => '2345678'
-  fill_in :user_password_confirmation, :with => '2345678'
+  fill_in :user_email, :with => 'usqw@example.ru'
+  fill_in :user_username, :with => 'egor'
+  fill_in :user_password, :with => '3456789'
+  fill_in :user_password_confirmation, :with => '3456789'
 
   click_button 'Sign up'
 end
